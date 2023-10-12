0442eb25c613a9bba4c403b3c86e3eb50c714a1a
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-12 18:36:20 -0400

    asd

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -6,15 +6,32 @@ const Home = () => {
       <h1 className="title">About Me</h1>
       <p className="aboutme">
         <span className="paragraph">
-          I am a web developer based out of South Eastern Connecticut. I love to
-          develope modern and amazing looking websites.
+          I am a web developer, currently attending trade school for
+          Informational Technology.
+        </span>
+        <span className="paragraph">
+          I love developing websites and I hope to learn and develope your dream
+          site! Since I am a young developer, I don't charge a lot. Contact me
+          with any inqueries you may have about website you want/need created.
         </span>
       </p>
       <div className="skills">
-        <div className="skill">HTML</div>
-        <div className="skill">CSS</div>
-        <div className="skill">Javascript</div>
-        <div className="skill">Html</div>
+        <div className="skill">
+          <span className="text">HTML</span>
+          <span className="hover"></span>
+        </div>
+        <div className="skill">
+          <span className="text">CSS</span>
+          <span className="hover"></span>
+        </div>
+        <div className="skill">
+          <span className="text">Javascript</span>
+          <span className="hover"></span>
+        </div>
+        <div className="skill">
+          <span className="text">Html</span>
+          <span className="hover"></span>
+        </div>
       </div>
     </div>
   );
