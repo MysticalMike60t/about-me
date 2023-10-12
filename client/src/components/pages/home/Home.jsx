252efca431accda81c2829226d9b57a71640528c
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-12 18:24:47 -0400

    sd

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="title">Home</h1>
+      <h1 className="title">About Me</h1>
+      <p className="aboutme">
+        <span className="paragraph">
+          I am a web developer based out of South Eastern Connecticut. I love to
+          develope modern and amazing looking websites.
+        </span>
+      </p>
+      <div className="skills">
+        <div className="skill">HTML</div>
+        <div className="skill">CSS</div>
+        <div className="skill">Javascript</div>
+        <div className="skill">Html</div>
+      </div>
     </div>
   );
 };
